33e65e8951f689acb0da9514065411860e5ad847
SabrinaMannan <sabrinameem21@gmail.com>
2022-03-04 02:00:25 +0600

    details button added

--- js/mobile.js
+++ js/mobile.js
@@ -10,5 +10,58 @@ const searchPhone = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displaySearchResult(data.data));
+}
+
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        
+        <div onclick="loadPhoneDetail('${data.slug}')" class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${data.phone_name}</h5>
+            <p class="card-text">${data.brand}</p>
+        </div>
+    </div>
+   
+        `;
+        searchResult.appendChild(div);
+    })
+
+}
+
+const loadPhoneDetail = slug => {
+
+    const url = `https://openapi.programming-hero.com/api/phone/'${data.slug}'`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data[0]));
+}
+
+const displayPhoneDetail = data => {
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+
+    <img src="${data.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${data.phone_name}</h5>
+        
+
+        <p class="card-text">${data.releaseDate}</p>
+        <p class="details"> ${data.mainFeatures}</p>
+        <a href="#" class="btn btn-primary">
+        Details</a>
+    </div>
+    `;
+
+    phoneDetails.appendChild(div);
 }
